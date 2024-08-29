4b7dbf1f44af96ef9f3df97be21a8476609bc09c
Vincent <yellow.mongoose@posteo.net>
2024-08-30 00:04:57 +0200

    Added responsive card grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default async function Home() {
 
   return (
     <main>
-      <div className='grid grid-cols-3 gap-8'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-8'>
         {mtgcards.map(mtgcard => (
           <Card
             key={mtgcard.id}
